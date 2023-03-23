7e43bdb3651727e15692f7ea0bceb16b7fe0259d
sumaiya haseeb <sumaiyaayaz71@gmail.com>
2023-03-23 12:40:32 +0530

    performing crud operations using automation

---
+++ src/test/DeleteExample.java
@@ -0,0 +1,35 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DeleteExample {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce", "root", "root");
+
+		Statement stmt = con.createStatement();
+		stmt.executeUpdate("delete from eproduct where name='Television';");
+
+		System.out.println("Product deleted ");
+
+		ResultSet result = stmt.executeQuery("select * from eproduct");
+
+		while (result.next()) {
+
+			System.out.println("Product Id " + result.getInt("ID"));
+			System.out.println("Product name " + result.getString("name"));
+			System.out.println("Price is " + result.getDouble("price"));
+		}
+
+		con.close();
+
+	}
+
+}
